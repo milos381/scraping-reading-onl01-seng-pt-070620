62ebc14faeb4cbb7b656d46172a40379fdc4c0cc
Milos Popovic <milos_popovic@outlook.com>
2020-08-03 22:56:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
+# doc = Nokogiri::HTML(html)
+# puts doc
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".headline-260IBN").text
