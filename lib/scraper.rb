668a1ad858624c0a0443609c2b07017436045ff9
Milos Popovic <milos_popovic@outlook.com>
2020-08-04 00:39:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 puts doc.css(".headline-26OIBN").text.strip
-courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+courses = doc.css(".gridContainer-EnvccM..inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
  
 courses.each do |course|
   puts course.text.strip
